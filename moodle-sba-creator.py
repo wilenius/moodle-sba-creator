c9d555949b3dfa4d6a56de370dcfc674d006c21d
Heikki Wilenius <heikki.wilenius@iki.fi>
2023-03-16 07:22:19 +0200

    Improved error reporting and exception handling.

--- moodle-sba-creator.py
+++ moodle-sba-creator.py
@@ -1,77 +1,130 @@
 import sys
 import openai
+from pathlib import Path
+import traceback
+import logging
 
-openai.api_key = "YOUR API KEY HERE"
+# PARAMETERS: 1. topic file (list of question topics separated by newline) 2. Number of questions per topic 3. name of output xml file
+# Additionally, sample_question.txt for a question to prime the gpt api. If the file doesn't exist, the default sample question is used instead.
 
-topic = str(sys.argv[1])
+topicfile = str(sys.argv[1])
+openai.api_key = "API KEY HERE"
+
+topics = Path(topicfile).read_text()
+topics_list = topics.split("\n")
+xml_header = '<?xml version="1.0"?>\n<quiz>\n'
+
 iterations = int(sys.argv[2])
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-0301",
-    messages=[
-        {"role": "system", "content": "You are a helpful teacher's assistant that creates multiple choice questions based on the topics given by the user."},
-        {"role": "system", "name":"example_user", "content": f"""I need you to print {iterations} separate multiple choice questions with four alternative answers, the correct answer being the fourth answer. The third answer should be close to a correct answer, but still unequivocally wrong. The topic of the question is selected based on the user input. The output format is correctly formatted Moodle Quiz XML, here's an example of the format:
+sample_question_file = Path("sample_question.txt")
 
-        	<question type="multichoice">
-			<name>
-				<text>Question name</text>
-			</name>
-			<questiontext format="html">
-				<text>Question text</text>
-			</questiontext>
-			<answer fraction="0">
-    			<text>Alternative answer 1</text>
-   				<feedback><text>Incorrect.</text></feedback>
-			</answer>
-			<answer fraction="0">
-    			<text>Alternative answer 2</text>
-   				<feedback><text>Incorrect.</text></feedback>
-			</answer>
-			<answer fraction="0">
-				<text>Alternative answer 3</text>
-				<feedback><text>Almost correct.</text></feedback>
-			</answer>
-			<answer fraction="100">
-    			<text>Alternative answer 4</text>
-   			<feedback><text>Correct.</text></feedback>
-			</answer>
-			</question>
+if sample_question_file.is_file():
+    sample_question = Path(sample_question_file).read_text()
+    print("Custom sample question file found, using it to generate questions.")
+else:
+    sample_question = """
+		<question type="multichoice">
+				<name>
+					<text>Borneon historia 1</text>
+				</name>
+				<questiontext format="html">
+					<text>Milloin alkaa dokumentoitu historia Borneon saaren ja Kiinan valtakunnan välillä?</text>
+				</questiontext>
+				<answer fraction="0">
+					<text>1600-luvulla, kun Länsi-Borneon sulttaanit toivat työläisiä Kiinasta työskentelemään kaivoksissa.</text>
+						<feedback><text>Väärin. Työläisten maahanmuutto Kiinasta alkoi 1700-luvulla. Lisäksi on olemassa myös aikaisempia historiallisia lähteitä.</text></feedback>
+				</answer>
+				<answer fraction="0">
+					<text>1700-luvulla, kun Länsi-Borneon sulttaanit toivat työläisiä Kiinasta työskentelemään kaivoksissa.</text>
+						<feedback><text>Väärin. Työläisten maahanmuutto Kiinasta alkoi 1700-luvulla, mutta on olemassa myös aikaisempia historiallisia lähteitä.</text></feedback>
+				</answer>
+				<answer fraction="0">
+					<text>1300-luvulla</text>
+					<feedback><text>Melkein oikein. 1300-luvulla Brunein sulttaani vaihtoi uskollisuutensa Majapahitista Kiinan valtakuntaan, mutta on olemassa varhaisempiakin kiinalaisia historiallisia lähteitä, jotka mainitsevat Borneon.</text></feedback>
+				</answer>
+				<answer fraction="100">
+					<text>800-luvulla</text>
+					<feedback><text>Oikein. Kiinalainen käsikirjoitus vuodelta 977 mainitsee saaren nimeltä "P'o-ni".</text></feedback>
+				</answer>
+		</question>
+    """
+    print("Sample question file not found. Using the default sample question")
 
-			The first question topic is: history of Borneo"""
-        },
-        {"role": "system", "name": "example_assistant", "content": """
-        	<question type="multichoice">
-			<name>
-				<text>History of Borneo 1</text>
-			</name>
-			<questiontext format="html">
-				<text>When does the documented history with the island of Borneo and the imperial China begin?</text>
-			</questiontext>
-			<answer fraction="0">
-    			<text>In the 17th century, when the sultans of Western Borneo imported laborers from China to work in mines.</text>
-   				<feedback><text>Incorrect. The immigration of laborers started in the 18th century. Also, there are much earlier historical sources.</text></feedback>
-			</answer>
-			<answer fraction="0">
-    			<text>In the 18th century, when the sultans of Western Borneo imported laborers from China to work in mines.</text>
-   				<feedback><text>Incorrect. The sultans did import laborers, but there are earlier historical sources.</text></feedback>
-			</answer>
-			<answer fraction="0">
-				<text>In the 14th century</text>
-				<feedback><text>Almost correct. In the 14th century, the Sultan of Brunei changed his allegiance from Majapahit to imperial China, but there are earlier Chinese historical sources that mention the island.</text></feedback>
-			</answer>
-			<answer fraction="100">
-    			<text>In the 9th century.</text>
-   			<feedback><text>Correct. A Chinese manuscript from the year 977 mentions the island of "P'o-ni".</text></feedback>
-			</answer>
-			</question>"""},
-        {"role": "system", "name":"user", "content": f"""Now, output {iterations} unique questions on this topic: {topic}."""},
-    ],
-    temperature=0,
-    top_p=1,
-)
+filepath = Path(sys.argv[3])
+with filepath.open("w", encoding ="utf-8") as f:
+    f.write(xml_header)
+    f.close()
 
-result = ''
-for choice in response.choices:
-    result += choice.message.content
+for topic in topics_list:
+	print("Generating questions for topic: " + topic)
+	try:
+		response = openai.ChatCompletion.create(
+		    model="gpt-3.5-turbo-0301",
+		    messages=[
+		        {"role": "system", "content": "You are a helpful teacher's assistant that creates multiple choice questions based on the topics given by the user. You communicate in the Finnish language"},
+		        {"role": "system", "name":"example_user", "content": f"""Haluan, että tulostat {iterations} erillistä monivalintakysymystä, joissa on neljä vaihtoehtoista vastausta. Neljäs vastaus on oikea vastaus. Kolmas vastaus on lähellä oikeaa vastausta, mutta silti yksiselitteisesti väärin. Kysymyksen aihe valitaan käyttäjän syötteen perusteella. Vastaus on Moodlen Tentti-aktiviteetin XML-formaatin mukainen, tässä on esimerkki formaatista:
 
-print(result)
+		        	<question type="multichoice">
+					<name>
+						<text>Kysymyksen nimi</text>
+					</name>
+					<questiontext format="html">
+						<text>Kysymyksen teksti</text>
+					</questiontext>
+					<answer fraction="0">
+		    			<text>Vastausvaihtoehto 1</text>
+		   				<feedback><text>Väärin.</text></feedback>
+					</answer>
+					<answer fraction="0">
+		    			<text>Vastausvaihtoehto 2</text>
+		   				<feedback><text>Väärin.</text></feedback>
+					</answer>
+					<answer fraction="0">
+						<text>Vastausvaihtoehto 3</text>
+						<feedback><text>Melkein oikein.</text></feedback>
+					</answer>
+					<answer fraction="100">
+		    			<text>Vastausvaihtoehto 4</text>
+		   			<feedback><text>Oikein.</text></feedback>
+					</answer>
+					</question>
+					"""
+		        },
+		        {"role": "system", "name": "example_assistant", "content": f"""
+		        	Tässä on haluamasi monivalintakysymys:
+		        	{sample_question}
+
+		        	"""},
+		        {"role": "system", "name":"user", "content": f"""Nyt, tulosta {iterations} kappaletta uniikkeja kysymyksiä tästä aiheesta: {topic}."""},
+		    ],
+		    temperature=0,
+		    top_p=1,
+		)
+	except Exception as e:
+		print("failed to generate for topic: " + topic)
+		print(e)
+		continue
+	
+	result = ''
+	xml_cleaned = ''
+
+	for choice in response.choices:
+	    result += choice.message.content
+
+
+	# create valid html
+	
+	for line in result.splitlines():
+		if line.strip().startswith("<"):
+			xml_cleaned += line + "\n"
+
+
+	with filepath.open("a", encoding ="utf-8") as f:
+		f.write(xml_cleaned)
+		f.close()
+
+# loop done, close xml
+
+with filepath.open("a", encoding ="utf-8") as f:
+    	f.write("</quiz>\n")
+    	f.close()
